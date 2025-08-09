943a05572a13893af031c3e0e3e4dbf94ec36eb7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-09 18:22:19 +0200

    Create basic view builder class

--- projects/angular-ui-generator/src/lib/decorator/register/register-view.decorator.ts
+++ projects/angular-ui-generator/src/lib/decorator/register/register-view.decorator.ts
@@ -1,7 +1,8 @@
-import {RegisterViewDto} from '../../model/dto/register-view.dto';
+import {RegisterViewDto} from '../../model/dto/register/register-view.dto';
+import {ViewBuilder} from '../../builder/view.builder';
 
 export function RegisterView(dto: RegisterViewDto) {
-  return function (target: Function) {
+  return function (target: typeof ViewBuilder) {
     console.log('RegisterView', target, dto);
   }
 }
--- projects/angular-ui-generator/src/public-api.ts
+++ projects/angular-ui-generator/src/public-api.ts
@@ -1,1 +1,2 @@
+export * from './lib/builder/view.builder';
 export * from './lib/decorator/register/register-view.decorator';
