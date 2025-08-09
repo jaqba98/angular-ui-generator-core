db97d7478c4b87293fd0f30a4420dc456bc74773
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-09 18:31:07 +0200

    Run the build method in the ngOnInit method

--- projects/angular-ui-generator/src/lib/builder/view.builder.ts
+++ projects/angular-ui-generator/src/lib/builder/view.builder.ts
@@ -1,3 +1,13 @@
-export abstract class ViewBuilder {
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'lib-view-builder',
+  template: ''
+})
+export abstract class ViewBuilder implements OnInit {
+  ngOnInit() {
+    this.build();
+  }
+
   abstract build(): void;
 }
