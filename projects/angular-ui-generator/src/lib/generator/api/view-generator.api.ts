f76d079646bfe93a44c1215a94edd58b24fba8cd
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-11 11:54:59 +0200

    Refactor the generator

--- projects/angular-ui-generator/src/lib/generator/api/view-generator.api.ts
+++ projects/angular-ui-generator/src/lib/generator/api/view-generator.api.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
-import { ViewGeneratorAbstract } from '../../shared/abstract/view-generator.abstract';
+import { ViewGeneratorAbstract } from '@angular-ui-generator/shared';
 
 @Component({
-  selector: 'lib-view-view.generator.ts',
+  selector: 'lib-view-generator',
   template: '',
 })
 export abstract class ViewGenerator implements OnInit, ViewGeneratorAbstract {
-  ngOnInit() {
+  ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
