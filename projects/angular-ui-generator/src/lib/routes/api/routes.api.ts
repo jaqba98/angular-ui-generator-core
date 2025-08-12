097574fb0c567c482f448a8d321588e4b77f334f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-12 06:52:51 +0200

    Create basic router builder

--- projects/angular-ui-generator/src/lib/routes/api/routes.api.ts
+++ projects/angular-ui-generator/src/lib/routes/api/routes.api.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TViewGenerator } from '../../shared';
 import { routesDomainBuilder } from '../builder/routes-domain.builder';
+import { routesBuilder } from '../builder/routes.builder';
 
 export const routes = (view: TViewGenerator): Routes => {
   const viewDomain = routesDomainBuilder(view);
-  console.log(viewDomain);
-  return [];
+  const routes = routesBuilder(viewDomain);
+  console.log(routes);
+  return routes;
 };
---
+++ projects/angular-ui-generator/src/lib/routes/builder/routes.builder.ts
@@ -0,0 +1,19 @@
+import { Route, Routes } from '@angular/router';
+import { RoutesDomainModel } from '../model/routes-domain.model';
+
+export const routesBuilder = (viewDomain: RoutesDomainModel): Routes => {
+  const route: Route = {
+    path: viewDomain.name,
+    // eslint-disable-next-line
+    component: viewDomain.target as any,
+    children: [],
+  };
+  viewDomain.children.forEach((child) => {
+    if (route.children) {
+      route.children = [...route.children, ...routesBuilder(child)];
+    } else {
+      route.children = [...routesBuilder(child)];
+    }
+  });
+  return [route];
+};
