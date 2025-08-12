d20f36e90a9f65180d31d1e510af58e2d4e616b7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-12 07:51:09 +0200

    Create basic generator generate only buttons

--- projects/angular-ui-generator/src/lib/generator/api/view-generator.api.ts
+++ projects/angular-ui-generator/src/lib/generator/api/view-generator.api.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { ViewGeneratorAbstract } from '../../shared';
 import { ViewElement } from '../model/view-element';
+import { ButtonComponent } from '../../ui';
 
 @Component({
   selector: 'lib-view-generator',
   template: '',
 })
-export abstract class ViewGenerator implements OnInit, ViewGeneratorAbstract {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+export abstract class ViewGenerator
+  implements AfterViewInit, ViewGeneratorAbstract
+{
+  @ViewChild('generator', { read: ViewContainerRef })
+  generator!: ViewContainerRef;
+
+  ngAfterViewInit() {
+    this.generator.clear();
+    this.generate().forEach(() => {
+      this.generator.createComponent(ButtonComponent);
+    });
   }
 
   abstract generate(): ViewElement[];
---
+++ projects/angular-ui-generator/src/lib/ui/button/button.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-button',
+  template: '<button>Hello</button>',
+})
+export class ButtonComponent {}
