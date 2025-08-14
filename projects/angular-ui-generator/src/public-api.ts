44ffabaf07c47ab7034c0180f242d3fae6a59345
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-14 08:21:35 +0200

    Refactor the main public-api.ts now it exports all subfolders

--- projects/angular-ui-generator/src/public-api.ts
+++ projects/angular-ui-generator/src/public-api.ts
@@ -1,4 +1,5 @@
 export * from './lib/generator/public-api';
 export * from './lib/register/public-api';
 export * from './lib/routes/public-api';
+export * from './lib/shared/public-api';
 export * from './lib/ui/public-api';
