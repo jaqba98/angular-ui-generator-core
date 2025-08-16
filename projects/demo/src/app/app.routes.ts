50d36bc7113ae2e26ba638c8faaecbbd77438eee
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-16 23:03:23 +0200

    Create very basic demo example

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { routesGenerator } from '../../../angular-ui-generator/src/public-api';
+import { HomeView } from './view/home.view';
+
+export const routes: Routes = routesGenerator(HomeView);
---
+++ projects/demo/src/app/view/home.view.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+import { ButtonBuilder, RegisterView, UiElement, ViewGenerator } from "../../../../angular-ui-generator/src/public-api";
+
+@Component({
+    selector: "app-home-view",
+    template: "<ng-template #viewGenerator></ng-template>"
+})
+@RegisterView({
+    name: "home",
+    children: []
+})
+export class HomeView extends ViewGenerator {
+    override generate(): UiElement[] {
+        return [
+            ButtonBuilder.build("Hello")
+        ];
+    }
+}
