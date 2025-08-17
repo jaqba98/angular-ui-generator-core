1a64475ce1c4e89f44e1c72b69abc47222473e96
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-17 12:22:35 +0200

    Add children to the block builder

--- projects/angular-ui-generator/src/lib/component/layout/block/model/block-builder.model.ts
+++ projects/angular-ui-generator/src/lib/component/layout/block/model/block-builder.model.ts
@@ -1,12 +1,10 @@
+import { BlockComponent } from '../component/block.component';
 import { BlockMetadataModel } from './block-metadata.model';
 
 export interface BlockBuilderModel {
   kind: 'block';
+  component: typeof BlockComponent,
   metadata: BlockMetadataModel;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  children: any[];
 }
-
-// export interface FlexBuilderModel {
-//   component: typeof FlexComponent;
-//   metadata: FlexMetadataModel;
-//   children: UiElement[];
-// }
