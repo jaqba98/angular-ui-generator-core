34eaf3e1d5e4cdb396968946be380c3f302cb9f4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-18 08:30:38 +0200

    Refactor the button control

--- projects/angular-ui-generator/src/lib/component/control/basic/button/button.component.ts
+++ projects/angular-ui-generator/src/lib/component/control/basic/button/button.component.ts
@@ -4,7 +4,6 @@ import { ButtonMetadataModel } from './button-metadata.model';
 @Component({
   selector: 'lib-component-control-basic-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   metadata = input.required<ButtonMetadataModel>();
