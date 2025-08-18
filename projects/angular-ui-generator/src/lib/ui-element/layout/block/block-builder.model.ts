f967f2e02594a19131a06450cca618ccdb59ebd7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-18 08:53:13 +0200

    Refactor the ui-elements

--- projects/angular-ui-generator/src/lib/ui-element/layout/block/block-builder.model.ts
+++ projects/angular-ui-generator/src/lib/ui-element/layout/block/block-builder.model.ts
@@ -1,8 +1,7 @@
-import { BlockComponent, BlockMetadataModel } from '../../../component';
+import { BlockComponent } from '../../../component';
 import { UiElement } from '../../type/ui-element';
 
 export interface BlockBuilderModel {
   component: typeof BlockComponent;
-  metadata: BlockMetadataModel;
   children: UiElement[];
 }
--- projects/angular-ui-generator/src/lib/ui-element/layout/block/block.builder.ts
+++ projects/angular-ui-generator/src/lib/ui-element/layout/block/block.builder.ts
@@ -1,12 +1,11 @@
-import { BlockComponent } from '../../../component';
 import { UiElement } from '../../type/ui-element';
 import { BlockBuilderModel } from './block-builder.model';
+import { BlockComponent } from '../../../component';
 
 export class BlockBuilder {
   static build(children: UiElement[]): BlockBuilderModel {
     return {
       component: BlockComponent,
-      metadata: {},
       children,
     };
   }
